e327de15bbfeeb2887d1b38bd37d7b54b28ae743
Alpha <ravalasonn@gmail.com>
2019-08-01 21:42:32 +0300

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+for i in (5..104)
+	film = Movie.find(i)
+	film.synopsis = Faker::Lorem.sentence(word_count: 10, supplemental: true)
+	film.save
+end
+# genre = ["action", "horreur", "comédie", "drame"]
+# 100.times do |index|
+# 	Movie.create(
+# 		name: Faker::Game.title,
+# 		year: Faker::Date.between(from: 1900, to: 2020),
+# 		genre: genre[rand(4)],
+# 		synopsis: Faker::Lorem.sentence(word_count: 10, supplemental: true),
+# 		director: Faker::Name.first_name + Faker::Name.last_name,
+# 		allocine_rating: (Faker::Number.between(from: 0, to: 5.0)).round(1),
+# 		my_rating: nil,
+# 		already_seen: false
+# 	)
+# end
